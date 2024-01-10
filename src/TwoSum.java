bf229c01d5b87363f4d512a913c3abe66ac78799
santhosh116 <santhosh1161999@gmail.com>
2024-01-10 15:26:19 +0530

    Second commit
    Take input from runtime

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,10 +1,17 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {3, 3};
-        int target = 6;
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
         int[] indices = solve(arr, target);
         System.out.println(indices[0] + " " + indices[1]);
     }
@@ -13,7 +20,7 @@ public class TwoSum {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < arr.length; i++) {
             int complement = target - arr[i];
-            if(map.containsKey(complement)) return new int[]{i, map.get(complement)};
+            if(map.containsKey(complement)) return new int[]{map.get(complement), i};
             map.put(arr[i], i);
         }
         return new int[]{0, 0};
