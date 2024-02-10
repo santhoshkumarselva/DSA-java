b4f4510902ea8b083e0caa530b390b891fff94d0
Santhosh <santhoshkumarselvaraj205@gmail.com>
2024-02-11 02:55:48 +0530

    ContainsDuplicate
    Java solution for ContainsDuplicate problem.

---
+++ src/main/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int n;
+        int[] arr;
+        System.out.println("Enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        if(solve(arr, n)) {
+            System.out.println("The array contains duplicate");
+        } else {
+            System.out.println("The array doesn't contain any duplicate value");
+        }
+    }
+
+    private static boolean solve(int[] arr, int n) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++) {
+            if(set.contains(arr[i])) return true;
+            else set.add(arr[i]);
+        }
+        return false;
+    }
+}
